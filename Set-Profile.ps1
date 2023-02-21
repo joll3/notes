9ba94f047e35674028ec2d1a5de0334c8091a59b
Jolla <5497641+Joll3@users.noreply.github.com>
2023-02-21 21:16:32 +0200

    update countermeasures

--- Set-Profile.ps1
+++
@@ -1,6 +0,0 @@
-# $profilePath contains URL to profile.ps1 
-$profilePath = 'https://raw.githubusercontent.com/joll3/azure/profile.ps1'
-# New random GUID to prevent web client from caching file 
-$downloadString = '{0}?{1}' -f $profilePath, (New-Guid)
-# Download profile.ps1 as string and execute to load into runspace
-Invoke-Expression ((New-Object System.Net.WebClient).DownloadString($profilePath))
--- profile.ps1
+++
@@ -1,3 +0,0 @@
-function Show-HelloWorld {
-    Write-Host "hello, world!"
-}
